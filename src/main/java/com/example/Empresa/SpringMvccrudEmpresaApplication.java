7fcd320d4a258358554eff862f0dc2b835809685
llruiz <llanosrzm@gmail.com>
2023-03-23 17:01:26 +0100

    Estado inicial del proyecto 2

--- src/main/java/com/example/Empresa/SpringMvccrudEmpresaApplication.java
+++ src/main/java/com/example/Empresa/SpringMvccrudEmpresaApplication.java
@@ -1,4 +1,4 @@
-package com.example.springMVCCRUDEmpresa;
+package com.example.Empresa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/example/services/DepartamentoServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.services;
+
+public class DepartamentoServiceImpl implements DepartamentoService {
+    
+}
