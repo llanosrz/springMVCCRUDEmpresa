ca2c127bc906cf403da90f8e80b94dcbb1e246c1
llruiz <llanosrzm@gmail.com>
2023-03-23 14:07:35 +0100

    Estado inicial del proyecto

---
+++ src/test/java/com/example/springMVCCRUDEmpresa/SpringMvccrudEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springMVCCRUDEmpresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvccrudEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
